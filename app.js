23e21bf54466b4790527b5f87677089600e03ca2
escandon100 <olakunle23odunade@gmail.com>
2025-07-14 13:27:56 +0100

    initial commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(bodyParser.json());
 
-app.use(cors({origin:"http://localhost:5173",credentials : true}))
+app.use(cors({origin:"https://www.ivalidatecards.com",credentials : true}))
 
 
 
